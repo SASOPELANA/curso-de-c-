c457130ad3c47be68f3a5ff317fc1b6246f6492a
sergio <sopekof@gmail.com>
2024-01-15 00:51:38 -0300

    Actulizar modulo 8

---
+++ Curso de C# Maxi Programa. Basico/Unidad8/FuncionSuma/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace FuncionSumar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        
+        int n1, n2, resultado;
+
+        Console.WriteLine("Ingrese el primer numero: ");
+        n1 = int.Parse(Console.ReadLine());
+        Console.WriteLine("Ingrese el segundo numero: ");
+        n2 = int.Parse(Console.ReadLine());
+
+        // Logica.
+        // resultado = n1 + n2;
+        resultado = sumar(n1, n2);
+        // Fin logica.
+
+        Console.WriteLine("El resultado es: " + resultado);
+        // SCOPE.
+           
+
+        }
+
+        static int sumar(int a, int b){
+            int r;
+            r = a + b;
+            return r;
+        }
+
+
+    }
+}
---
+++ Curso de C# Maxi Programa. Basico/Unidad8/IntroFuncion/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace IntroFuncion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        
+        // Aqui va el codigo.
+        // Funciones.
+        // Partes de la función.    
+        // Cabecera.
+        // 1. Tipo de datos.
+        // 2. Nombre.
+        // 3. Opcional: parametros.
+        // Cuerpo.
+        // Definicion / Logica.
+        // Opcional: retorno de valores / Resultados.
+        // int n =  devolverUno();
+
+        Console.WriteLine("Fin del programa: " + devolverUno()); // asi se llama a la funcion.
+
+        }
+
+        static int devolverUno(){  // Funcion para a llamar. Siempre fuera de la funcion principal Main, fuera de sus llaaves del programa.
+            int numero = 500;
+            return numero;
+        }
+    }
+}
